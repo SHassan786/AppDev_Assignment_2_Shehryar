6818fef9b936220091350cad6b229c2f193635fd
Syed Ali Mashoud <mashoudntaha@gmail.com>
2023-10-29 11:19:58 +0500

    login page completed

---
+++ lib/loginpage.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 247, 247, 247),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 17.0 / 19.0,
+              child: Image.asset('assets/asset1.jpg'),
+            ),
+            SizedBox(
+              width: 320,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 300,
+                      child: Text('Log In',
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Color.fromARGB(255, 118, 93, 194),
+                          )),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 118, 93, 194),
+                          ),
+                        ),
+                        labelText: 'Username',
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(8, 20, 8, 8),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 118, 93, 194),
+                          ),
+                        ),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 320,
+                    height: 70,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('Hello');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 118, 93, 194),
+                        ),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                        child: Row(
+                          children: [
+                            const Text("Don't have an account?"),
+                            TextButton(
+                                onPressed: () {
+                                  print('Hello');
+                                },
+                                child: const Text("Sign Up")),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            print("object");
+                          },
+                          child: const Text('Forgot Password?')),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
